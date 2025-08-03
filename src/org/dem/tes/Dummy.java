1a00ef29e92d4800dd66dd0c783f2b34ffdb3d3f
greensgit <rajgreensomr@gmail.com>
2025-08-03 11:21:19 +0530

    new code added

---
+++ src/org/dem/tes/Dummy.java
@@ -0,0 +1,64 @@
+package org.dem.tes;
+
+public class Dummy {
+
+public static void main(String[] args) {
+	
+	System.out.println("This is demo");
+	System.out.println("This is yuvaraj branch");
+}
+private void login() {
+	System.out.println("This is Azar branch");
+}
+
+private void addToKart() {
+	System.out.println("This is anbarasu working on add to kart");
+}
+private void payment() {
+	System.out.println("This is Anish - working on Payment module");
+}
+
+private void Confirmation() {
+	System.out.println("This is satham");
+}
+
+private void search() {
+System.out.println("This is Joe branch - working on search");
+}
+
+private void cancellation() {
+	System.out.println("This is deepti - working on cancellattion");
+}
+
+private void creditCard() {
+	System.out.println("This is hari - working on credit card");
+}
+
+private void netbanking() {
+	System.out.println("This is selva - working on netbanking");
+}
+
+private void forgotPassword() {
+	System.out.println("This is velu - working on forgot password");
+}
+
+private void changePassword() {
+	System.out.println("This is vasugi - working on change password");
+}
+
+private void removeKart() {
+	System.out.println("This is Ahmed - working on remove kart");
+}
+
+private void disableKart() {
+System.out.println("This is sujitha - working on disable kart");
+}
+
+private void gpayUPI() {
+	System.out.println("This is ajith - working on gpay");
+}
+
+private void cashOnDelivery() {
+	System.out.println("This is aiswarya - working on cash");
+}
+}
